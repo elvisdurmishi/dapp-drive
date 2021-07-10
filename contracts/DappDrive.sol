72663dc5476bee1aaa4243b593fc939a0319bdb7
Elvis Durmishi <elvisdurmishi@gmail.com>
2021-07-10 10:29:27 +0200

    smart contract

--- contracts/DappDrive.sol
+++ contracts/DappDrive.sol
@@ -13,5 +13,13 @@ contract DappDrive {
     function add(string memory _hash, string memory _fileName, string memory _fileType, uint _date) public {
         files[msg.sender].push(File({hash: _hash, fileName: _fileName, fileType: _fileType, date: _date}));
     }
-    
+
+    function getFile(uint _index) public view returns(string memory, string memory, string memory, uint){
+        File memory file = files[mgs.sender][_index];
+        return (file.hash, file.fileName, file.date);
+    }
+
+    function getLength() public view returns(uint){
+        return files[msg.sender].length;
+    }
 }

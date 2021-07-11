77f83da1bf97bb523b732b7df0d1aafbf73c7986
Elvis Durmishi <elvisdurmishi@gmail.com>
2021-07-11 02:09:17 +0200

    deployment infura contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +11,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,       // Ropsten's id
+      gas: 300000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
   },
 };
